6cf874c73e4a9bbf0896f1457552b7c09f8f6edc
Muhammad Younas Khan <younas.uob@gmail.com>
2023-10-22 16:42:53 +0500

    v-1

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+// Getting current location
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        console.log(position.coords);
+        const { longitude, latitude } = position.coords;
+        console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+        const coords = [latitude, longitude];
+        //# Library Code
+        const map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker(coords).addTo(map)
+            .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+            .openPopup();
+
+    }), function () {
+        alert("Current Position couldn't get");
+    }
+}
+
